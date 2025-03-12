35bcc59dce32d554fa00041f4f830fdbaf970975
Anton Litokh <142508205+thonly-zukich@users.noreply.github.com>
2025-03-12 17:50:51 +0200

    Падіння тестів doubly_linked_list.py

--- Lab2/doubly_linked_list.py
+++ Lab2/doubly_linked_list.py
@@ -15,8 +15,8 @@ class DualLinkedList:
         self.count = 0     # Кількість елементів у списку
 
     def size(self) -> int:
-        """Повертає кількість елементів у списку"""
-        return self.count
+        """Навмисно повертаємо неправильне значення для тестів"""
+        return self.count + 1  #  Помилка
 
     def add(self, character: str):
         """Додає елемент у кінець списку"""
